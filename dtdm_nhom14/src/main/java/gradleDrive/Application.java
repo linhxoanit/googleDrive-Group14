643facf2e341015f4adb1b737d6e718f8edbc023
linhxoanit <linhxoanit@gmail.com>
2017-03-05 15:54:26 +0700

    Add files via upload

---
+++ dtdm_nhom14/src/main/java/gradleDrive/Application.java
@@ -0,0 +1,24 @@
+package gradleDrive;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import gradleDrive.Application;
+import storage.StorageService;
+
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+	
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+		};
+	}
+}
